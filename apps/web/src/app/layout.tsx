fe1ddd395adc3c3e520e5ad07d057fc185cf07a3
Anik Vox <anikvox@proton.me>
2026-02-04 09:25:26 +0530

    fix: hydration issue on web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body>{children}</body>
       </html>
     </ClerkProvider>
